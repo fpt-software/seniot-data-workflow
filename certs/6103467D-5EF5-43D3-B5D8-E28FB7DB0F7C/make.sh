4fcac75f6eaab64c36e778c04345dc81859769d7
Duong Dinh Cuong <cuongdd1@Duongs-MacBook-Pro.local>
2015-12-25 20:32:37 -0600

    add script autogen

--- certs/6103467D-5EF5-43D3-B5D8-E28FB7DB0F7C/make.sh
+++ certs/6103467D-5EF5-43D3-B5D8-E28FB7DB0F7C/make.sh
@@ -1,2 +1,3 @@
 openssl genrsa -out client-key.pem 4096
 openssl req -new -config client.cnf -key client-key.pem -out client-csr.pem
+openssl x509 -req -extfile client.cnf -days 999 -passin "pass:password" -in client-csr.pem -CA ../ca-crt.pem -CAkey ../ca-key.pem -CAcreateserial -out client-crt.pem  

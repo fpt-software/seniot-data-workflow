6aca937a69fce3ccabe0b43888147e543574ab04
Duong Dinh Cuong <cuongdd1@Duongs-MacBook-Pro.local>
2015-12-29 13:17:53 -0600

    update

--- nodes/core/https/https.js
+++ nodes/core/https/https.js
@@ -58,7 +58,7 @@ module.exports = function(RED) {
 			self.on("input", function(msg) {
 				self.method = msg.method || self.method;
 				self.url = msg.url || self.url;
-				console.log("httpTlsRequestNode", self.url, self.method);
+				console.log("httpTlsRequestNode", self.url, self.method, self.certificate.agentOptions);
 				request({
 					url : self.url,
 					method : self.method,

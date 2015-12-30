066a6d29ab53d31292db601396d09b576ec40422
Duong Dinh Cuong <cuongdd1@Duongs-MacBook-Pro.local>
2015-12-30 16:38:31 -0600

    update

--- nodes/core/azure-https/azure-https.js
+++ nodes/core/azure-https/azure-https.js
@@ -62,7 +62,7 @@ module.exports = function(RED) {
 				deferred.resolve(null);
 			}
 			RED.httpNode.post("/azure/https/init", function(req, res) {
-				console.log("HELLO", JSON.parse(req.body));
+				console.log("HELLO", req.body);
 				if (req.body) {
 					var options = JSON.parse(req.body);
 					var connectionString = 'HostName=' + options.HostName + ';DeviceId=' + options.DeviceId + ';SharedAccessKeyName=' + options.SharedAccessKeyName + ';SharedAccessKey=' + options.PrimaryKey + '';

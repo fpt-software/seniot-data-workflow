85eab8bb17ba683aa72b1181985d8bb8869a8b6a
Duong Dinh Cuong <cuongdd1@Duongs-MacBook-Pro.local>
2015-12-30 16:29:31 -0600

    update

--- nodes/core/azure-https/azure-https.js
+++ nodes/core/azure-https/azure-https.js
@@ -21,6 +21,7 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
  */
+var express = require("express");
 
 module.exports = function(RED) {
 	"use strict";

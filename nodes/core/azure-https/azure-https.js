dbd8dd657ac245dae1a9f7f8b4b9ec46fd26fa8d
Duong Dinh Cuong <cuongdd1@Duongs-MacBook-Pro.local>
2016-01-01 15:18:47 -0600

    update azure https

--- nodes/core/azure-https/azure-https.js
+++ nodes/core/azure-https/azure-https.js
@@ -120,7 +120,7 @@ module.exports = function(RED) {
 							var connectionString = 'HostName=' + data.HostName + ';DeviceId=' + data.DeviceId + ';SharedAccessKey=' + data.PrimaryKey + '';
 							node.log("Initiate Azure IoT Hub HTTPS node for " + node.deviceId + ", " + connectionString);
 							var device = new Client.fromConnectionString(connectionString);
-							var message = new device.Message(msg);
+							var message = new Device.Message(msg);
 							device.sendEvent(message, function(err, res) {
 								node.send({
 									err : err,

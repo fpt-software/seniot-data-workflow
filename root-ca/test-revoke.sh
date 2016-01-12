7fd63aa51fd9f893e1a29699d6b2619e9eb98c43
Duong Dinh Cuong <cuongdd1@fsoft.com.vn>
2016-01-12 01:08:52 +0000

    update storage

--- root-ca/test-revoke.sh
+++ root-ca/test-revoke.sh
@@ -1,1 +1,2 @@
-./revoke-client.sh FSOFTCA "fptsoftware" B103467D-5EF5-43D3-B5D8-E28FB7DB0F7F "fptlennox"
+./revoke-client.sh FSOFTCA "fptsoftware" $1 "fptlennox"
+cp FSOFTCA/ca-crl.pem /home/centos/api.go4smac.com/
